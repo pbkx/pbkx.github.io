71549ed9cbb5c04fef3b8007047da900869bfc05
mantisplosion <nanomantis07@gmail.com>
2023-06-10 23:01:57 -0500

    msg

--- phototextmaker.py
+++
@@ -1,8 +0,0 @@
-text = input("text: ")
-year = input("year: ")
-link = input("href: ")
-
-
-print("<img src=" + '''"''' + link + '''"''')
-print("alt=" + '''"''' + text + "; " + year + '''" ''' + '''width="500" height="500"><p>''' + text + "</p><br>")
-print("<br>")
